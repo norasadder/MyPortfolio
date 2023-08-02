1b6b1e2709189c6e377ea47395baaff29ff2b8a6
Nora Sadder <norasadder12@gmail.com>
2023-08-02 22:00:52 +0300

    edit the website to be resizable and changed the honors section design

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
 const EDUCATION_MAP_ID = "Education-Map";
 const SKILLS_CONTAINER_ID = "skills-container";
+const EDUCATION_TITLE_ID = "education-title";
+const SKILLS_TITLE_ID = "skills-title";
 
 function displayEducation() {
-    if (document.getElementById(EDUCATION_MAP_ID ).style.display == "none") {
-        document.getElementById(EDUCATION_MAP_ID ).style.display = "grid";
-        document.getElementById(SKILLS_CONTAINER_ID).style.display = "none";
-    }
-    else {
-        document.getElementById(EDUCATION_MAP_ID ).style.display = "none";
-        document.getElementById(SKILLS_CONTAINER_ID).style.display = "none";
-    }
-    
+  document.getElementById(EDUCATION_MAP_ID).style.display = "grid";
+  document.getElementById(SKILLS_CONTAINER_ID).style.display = "none";
+  document.getElementById(EDUCATION_TITLE_ID).style.color =
+    "var(--purple-dark)";
+  document.getElementById(EDUCATION_TITLE_ID).style.fontWeight = "bold";
+  document.getElementById(SKILLS_TITLE_ID).style.color = "var(--purple-light)";
+  document.getElementById(SKILLS_TITLE_ID).style.fontWeight = "normal";
 }
 
 function displaySkills() {
-    if (document.getElementById(SKILLS_CONTAINER_ID).style.display == "none") {
-    document.getElementById(EDUCATION_MAP_ID).style.display = "none";
-    document.getElementById(SKILLS_CONTAINER_ID).style.display = "block";
-    }
-    else {
-        document.getElementById(EDUCATION_MAP_ID ).style.display = "none";
-        document.getElementById(SKILLS_CONTAINER_ID).style.display = "none";
-    }
+  document.getElementById(EDUCATION_MAP_ID).style.display = "none";
+  document.getElementById(SKILLS_CONTAINER_ID).style.display = "block";
+  document.getElementById(SKILLS_TITLE_ID).style.color = "var(--purple-dark)";
+  document.getElementById(SKILLS_TITLE_ID).style.fontWeight = "bold";
+
+  document.getElementById(EDUCATION_TITLE_ID).style.color =
+    "var(--purple-light)";
+  document.getElementById(EDUCATION_TITLE_ID).style.fontWeight = "normal";
 }
 
 function expandPL() {
-    if (document.getElementById("programming-lang-description").style.display == "none") {
+  if (
+    document.getElementById("programming-lang-description").style.display ==
+    "none"
+  ) {
     document.getElementById("PL-IMG").src = "images/Up.png";
-    document.getElementById("programming-lang-description").style.display = "grid";
-    }
-    else {
-        document.getElementById("PL-IMG").src = "images/down.png";
-        document.getElementById("programming-lang-description").style.display = "none";
-    }
+    document.getElementById("programming-lang-description").style.display =
+      "grid";
+  } else {
+    document.getElementById("PL-IMG").src = "images/down.png";
+    document.getElementById("programming-lang-description").style.display =
+      "none";
+  }
 }
 
 function expandOthers() {
-    if ( document.getElementById("others-description").style.display == "none") {
-        document.getElementById("othersIMG").src = "images/Up.png";
+  if (document.getElementById("others-description").style.display == "none") {
+    document.getElementById("othersIMG").src = "images/Up.png";
     document.getElementById("others-description").style.display = "grid";
-    }
-    else {
-        document.getElementById("othersIMG").src = "images/down.png";
-        document.getElementById("others-description").style.display = "none"
-    }
+  } else {
+    document.getElementById("othersIMG").src = "images/down.png";
+    document.getElementById("others-description").style.display = "none";
+  }
 }
